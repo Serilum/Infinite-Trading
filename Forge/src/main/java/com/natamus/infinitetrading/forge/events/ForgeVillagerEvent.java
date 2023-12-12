444e3cfdcbaa4fdcc05efcbeb381f2609f9cc564
Rick South <rick@natamus.com>
2023-12-12 09:50:12 +0100

    Infinite Trading source code for 1.19.2.

---
+++ Forge/src/main/java/com/natamus/infinitetrading/forge/events/ForgeVillagerEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.infinitetrading.forge.events;
+
+import com.natamus.infinitetrading.events.VillagerEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeVillagerEvent {
+	@SubscribeEvent
+	public void onVillagerClick(PlayerInteractEvent.EntityInteract e) {
+		VillagerEvent.onVillagerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null);
+	}
+}
