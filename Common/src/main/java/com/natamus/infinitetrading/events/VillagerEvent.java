6b006a9f52e1368b3f722f7d5bcd1ddc330ced38
Rick South <rick@natamus.com>
2023-12-12 09:50:48 +0100

    Infinite Trading source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/infinitetrading/events/VillagerEvent.java
@@ -0,0 +1,31 @@
+package com.natamus.infinitetrading.events;
+
+import com.natamus.collective.functions.EntityFunctions;
+import com.natamus.infinitetrading.config.ConfigHandler;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.WanderingTrader;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+
+public class VillagerEvent {
+	public static InteractionResult onVillagerClick(Player player, Level world, InteractionHand hand, Entity target, EntityHitResult hitResult) {
+		if (target instanceof Villager) {
+			if (ConfigHandler.villagerInfiniteTrades) {
+				Villager villager = (Villager) target;
+				EntityFunctions.resetMerchantOffers(villager);
+			}
+		}
+		else if (target instanceof WanderingTrader) {
+			if (ConfigHandler.wanderingTraderInfiniteTrades) {
+				WanderingTrader wanderer = (WanderingTrader)target;
+				EntityFunctions.resetMerchantOffers(wanderer);
+			}
+		}
+
+		return InteractionResult.PASS;
+	}
+}
