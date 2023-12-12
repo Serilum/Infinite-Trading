c0987f012672fdf9bdead4cadbccab4ee5caa198
Rick South <rick@natamus.com>
2023-12-12 09:50:22 +0100

    Infinite Trading source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/infinitetrading/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.infinitetrading.util;
+
+public class Reference {
+	public static final String MOD_ID = "infinitetrading";
+	public static final String NAME = "Infinite Trading";
+	public static final String VERSION = "4.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
