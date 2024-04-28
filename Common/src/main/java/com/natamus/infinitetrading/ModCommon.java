4783c96d550e4e899b695b30e8fa40988e3fa8fc
Rick South <rick@natamus.com>
2024-04-28 15:56:59 +0200

    Infinite Trading source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/infinitetrading/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.infinitetrading;
+
+import com.natamus.infinitetrading.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ Common/src/main/java/com/natamus/infinitetrading/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.infinitetrading.util;
+
+public class Reference {
+	public static final String MOD_ID = "infinitetrading";
+	public static final String NAME = "Infinite Trading";
+	public static final String VERSION = "4.3";
+	public static final String ACCEPTED_VERSIONS = "[1.20.5]";
+}
