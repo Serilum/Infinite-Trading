9d9f1b9047196badecc6b784c16b52b5a47664f9
Rick South <rick@natamus.com>
2024-07-28 07:27:36 +0200

    (v4.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/infinitetrading/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/infinitetrading/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.infinitetrading;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.infinitetrading.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.infinitetrading.neoforge.events.NeoForgeVillagerEvent;
 import com.natamus.infinitetrading.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
