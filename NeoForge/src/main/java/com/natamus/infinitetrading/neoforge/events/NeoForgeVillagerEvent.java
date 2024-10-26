d3d48a64614d66f39c9ef43bc2f4ae80e112f618
Rick South <rick@natamus.com>
2024-10-26 15:11:06 +0200

    Infinite Trading source code for 1.21.3.

---
+++ NeoForge/src/main/java/com/natamus/infinitetrading/neoforge/events/NeoForgeVillagerEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.infinitetrading.neoforge.events;
+
+import com.natamus.infinitetrading.events.VillagerEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeVillagerEvent {
+	@SubscribeEvent
+	public static void onVillagerClick(PlayerInteractEvent.EntityInteract e) {
+		VillagerEvent.onVillagerClick(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null);
+	}
+}
